81a9f6b02702daa31a0cc12fd2762e65d5f04c40
Karnav <karnavpopat@gmail.com>
2022-01-22 13:57:44 +0530

    refactor: db connection

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -6,8 +6,13 @@ mongoose.Promise = global.Promise;
 const db_url = ''.concat('mongodb+srv://', process.env.DB_USER!, ':', process.env.DB_PWD!, '@', process.env.DB_HOST!, '.w70wr.mongodb.net/recourse?retryWrites=true&w=majority');
 
 // connect to mongodb
-mongoose.connect(db_url)
-.then(() => console.log('db connected @ ' + process.env.DB_HOST!))
-.catch(err => console.log(err));
+mongoose.connect(db_url);
+const db_connection = mongoose.connection;
+
+// report success or failure
+db_connection.on('error', console.error.bind(console, 'db connection error:'));
+db_connection.once('open', () => {
+  console.log('db connected @ ' + process.env.DB_HOST!);
+});
 
 export {mongoose};
