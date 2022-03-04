b7231616699754f545b3d2a5f4f98bd8a45bd822
Karnav <karnavpopat@gmail.com>
2022-03-04 14:13:57 +0530

    style: prettier formatting

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -1,18 +1,24 @@
-import mongoose from 'mongoose';
-
-mongoose.Promise = global.Promise;
+import mongoose from "mongoose";
 
 // generate database url from .env
-const db_url = ''.concat('mongodb+srv://', process.env.DB_USER!, ':', process.env.DB_PWD!, '@', process.env.DB_HOST!, '.w70wr.mongodb.net/recourse?retryWrites=true&w=majority');
+const db_url: string = "".concat(
+    "mongodb+srv://",
+    process.env.DB_USER!,
+    ":",
+    process.env.DB_PWD!,
+    "@",
+    process.env.DB_HOST!,
+    ".w70wr.mongodb.net/recourse?retryWrites=true&w=majority"
+);
 
 // connect to mongodb
 mongoose.connect(db_url);
-const db_connection = mongoose.connection;
+const db_connection: any = mongoose.connection;
 
 // report success or failure
-db_connection.on('error', console.error.bind(console, 'db connection error:'));
-db_connection.once('open', () => {
-  console.log('db connected @ ' + process.env.DB_HOST!);
+db_connection.on("error", console.error.bind(console, "db connection error:"));
+db_connection.once("open", () => {
+    console.log("db connected @ " + process.env.DB_HOST!);
 });
 
-export {mongoose};
+export { mongoose };
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,43 +1,47 @@
-import http from 'http';
-import express, {Express} from 'express';
-import morgan from 'morgan';
-require('dotenv').config();
+import http from "http";
+import express, { Express } from "express";
+import morgan from "morgan";
+require("dotenv").config();
 
-import routes from './routes/routes';
+import routes from "./routes/routes";
 
 const router: Express = express();
 
 //logging
-router.use(morgan('dev'));
+router.use(morgan("dev"));
 // parse the request
-router.use(express.urlencoded({extended: false}));
+router.use(express.urlencoded({ extended: false }));
 // parse incoming json
 router.use(express.json());
 
-
 // boilerplate
-router.use( (request, response, next) => {
-
-	// set headers
-	response.header('Access-Control-Allow-Origin', '*');
-	response.header('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content-Type, Accept, Authorization');
-	if (request.method === 'OPTIONS') {
-		response.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
-		return response.status(200).json({});
-	}
-	next();
+router.use((request, response, next) => {
+    // set headers
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header(
+        "Access-Control-Allow-Headers",
+        "origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (request.method === "OPTIONS") {
+        response.header(
+            "Access-Control-Allow-Methods",
+            "GET PATCH DELETE POST"
+        );
+        return response.status(200).json({});
+    }
+    next();
 });
 
-router.use('/', routes);
+router.use("/", routes);
 
 // handles 404ing requests
-router.use( (request, response, next) => {
-	const error = new Error('not found');
-	return response.status(404).json({
-		message: error.message
-	});
+router.use((request, response, next) => {
+    const error: Error = new Error("not found");
+    return response.status(404).json({
+        message: error.message,
+    });
 });
 
-const server = http.createServer(router);
+const server: any = http.createServer(router);
 const PORT: any = process.env.PORT ?? 4004;
 server.listen(PORT, () => console.log(`express api @ ${PORT}`));
