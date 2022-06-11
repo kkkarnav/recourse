a64036936367b7b09f88b82c3bd402963db937af
Karnav <karnavpopat@gmail.com>
2022-06-12 02:29:43 +0530

    feat[API]: added an info page

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -3,8 +3,14 @@ import controller from "../controllers/controllers";
 
 const router: express.Router = express.Router();
 
+router.get("/", (request, response) => {
+	return response.status(200).sendfile('./index.html');
+});
 router.get("/api/course", controller.getCourse);
 router.get("/api/prof", controller.getProf);
 router.get("/api/review", controller.getReview);
+router.get('*', (request, response) => {
+	response.redirect('/');
+});
 
 export = router;
